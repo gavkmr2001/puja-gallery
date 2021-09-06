15c8978fc916204cd6d80625af34f469c376418e
Gaurav <gavkmr2001@gmail.com>
2021-09-06 10:10:15 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+
+
+
+dvnsmbnklnbl
+
+
+
+//------------------PORT-------------
+//.env file is not uploaded on github so we can 
+// keep sensititve info there 
+const port = process.env.PORT || 8000;
+
+//STARTING A SERVER---------------
+app.listen(port, () => {
+    console.log("app is running");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port= 8000
+
+app.listen(port, () => {
+    console.log("app is running")
+})
