b5b40765d45404036b4b5b145bf3eb2c1828d487
Gaurav <gavkmr2001@gmail.com>
2021-09-06 22:43:18 +0530

    basic

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
 
 
 
 
+// //------------------PORT-------------
+// //.env file is not uploaded on github so we can 
+// // keep sensititve info there 
+// const port = process.env.PORT || 8000;
 
-//------------------PORT-------------
-//.env file is not uploaded on github so we can 
-// keep sensititve info there 
-const port = process.env.PORT || 8000;
-
-//STARTING A SERVER---------------
-app.listen(port, () => {
-    console.log("app is running");
-});
-
+// //STARTING A SERVER---------------
+// app.listen(port, () => {
+//     console.log("app is running");
+// });
 
 
 
 
+console.log("hello")
 
 
 
+// const port= 8000
 
-
-
-
-
-const port= 8000
-
-app.listen(port, () => {
-    console.log("app is running")
-})
+// app.listen(port, () => {
+//     console.log("app is running")
+// })
